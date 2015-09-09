189e96ef61c7fddfd22e6ede352a88f9d1216d64
chomik <codecalm@gmail.com>
2015-09-09 16:52:39 +0200

    dots circle

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,38 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    connect = require('gulp-connect'),
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
     gulp.src('./*.scss')
         .pipe(sass({
             outputStyle: 'expanded'
         }).on('error', sass.logError))
-        .pipe(gulp.dest('./'));
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./'))
+        .pipe(connect.reload());
+});
+
+gulp.task('html', function () {
+    gulp.src('./*.html')
+        .pipe(connect.reload());
 });
 
 gulp.task('watch', ['sass'], function () {
     gulp.watch('./*.scss', ['sass']);
+    gulp.watch('./*.html', ['html']);
 });
+
+gulp.task('webserver', function() {
+    connect.server({
+        livereload: true,
+        root: ['.']
+    });
+});
+
+gulp.task('connect', ['webserver', 'watch']);
